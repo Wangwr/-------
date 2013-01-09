c20626361fcdd5d0d705954680485e64bc679c94
519968211 <519968211@localhost.(none)>
2013-01-09 12:35:13 +0800

    Initial Commit

---
+++ KRShareKit/XYZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XYZAppDelegate.h
+//  KRShareKit
+//
+//  Created by 519968211 on 13-1-9.
+//  Copyright (c) 2013年 519968211. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
